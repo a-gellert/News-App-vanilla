23e3b0a3545347b4b5bbf843c86698b37add6426
Александр Геллерт <sever6131@ya.ru>
2020-08-01 15:53:48 +0600

    final project

--- app.js
+++ app.js
@@ -53,6 +53,18 @@ function customHttp() {
         },
     };
 }
+//Elements
+
+const form = document.forms['newsControls'];
+const countrySelect = form.elements['country'];
+const categorySelect = form.elements['category'];
+const searchInput = form.elements['search'];
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    loadNews();
+});
+
 // Init http module
 const http = customHttp();
 
@@ -60,8 +72,8 @@ const newsService = (function () {
     const apiKey = 'c1a47377a22d4f819ef8903e00f483fb';
     const apiUrl = 'http://newsapi.org/v2';
     return {
-        topHeadlines(country = 'ru', cb) {
-            http.get(`${apiUrl}/top-headlines?country=${country}&apiKey=${apiKey}`, cb)
+        topHeadlines(country = 'ru', category = 'general', cb) {
+            http.get(`${apiUrl}/top-headlines?country=${country}&category=${category}&apiKey=${apiKey}`, cb)
         },
         everything(query, cb) {
             http.get(`${apiUrl}/everything?q=${query}&apiKey=${apiKey}`, cb)
@@ -75,36 +87,88 @@ document.addEventListener('DOMContentLoaded', function () {
     loadNews();
 });
 function loadNews() {
-    newsService.topHeadlines('ru', onGetResponse)
+    showLoader();
+    const country = countrySelect.value;
+    const category = categorySelect.value;
+    const search = searchInput.value;
+    if (!search) {
+        newsService.topHeadlines(country, category, onGetResponse);
+    } else {
+        newsService.everything(search, onGetResponse);
+    }
 }
 function onGetResponse(err, res) {
+    removeLoader();
+    if (err) {
+        showMessage(err, 'error-msg');
+        return;
+    }
+    if (!res.articles.length) {
+        showMessage('No news')
+        return;
+    }
     renderNews(res.articles)
 }
 
 function renderNews(news) {
     const newsContainer = document.querySelector('.news-container .row');
+    if (newsContainer.children.length) {
+        clearContainer(newsContainer);
+    }
     let fragment = '';
     news.forEach(newsItem => {
         const el = newsTemplate(newsItem);
         fragment += el;
     });
+    console.log(fragment);
     newsContainer.insertAdjacentHTML('afterbegin', fragment);
 }
 function newsTemplate({ urlToImage, title, url, description }) {
+
     return `
+    <div>
     <div class="col s12">
         <div class="card">
             <div class="card-image">
-                <img src="${urlToImage}">
+                <img src="${urlToImage || 'https://schastievkadre.ru/wp-content/uploads/2018/11/blog-1-2000x1125.jpg'}">
                 <span class="card-title">${title || ''}</span>
             </div>
             <div class="card-content">
                 <p>${description || ''}</p>
             </div>
             <div class="card-action">
-                <a href="${url}">Read more</a>
+                <a href="${url}'}">Read more</a>
             </div>
         </div>
     </div>
+    </div>
     `
+}
+
+function showMessage(msg, type = 'success') {
+    M.toast({ html: msg, classes: type });
+
+}
+function clearContainer(container) {
+    let child = container.lastElementChild;
+    while (child) {
+        container.removeChild(child);
+        child = container.lastElementChild;
+    }
+}
+
+function showLoader() {
+    document.body.insertAdjacentHTML(
+        'afterbegin',
+        `<div class ="progress">
+            <div class="indeterminate"></div>
+        </div>
+        `
+    );
+}
+function removeLoader() {
+    const loader = document.querySelector('.progress');
+    if (loader) {
+        loader.remove();
+    }
 }
